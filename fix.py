d87e0f45d846d5e0c5098ae9822d2b3f6567d0c8
jakpoudje <jakpoudje@gmail.com>
2026-04-30 20:22:44 +0100

    Delete fix.py

--- fix.py
+++
@@ -1,7 +0,0 @@
-content = open('index.html').read()
-content = content.replace('#aab-export-panel.open', '#aab-export-panel.open')
-# Find and fix the corrupted CSS selector
-import re
-content = re.sub(r'#\[aab-export-panel\.open\]\(http://aab-export-panel\.open\)', '#aab-export-panel.open', content)
-open('index.html', 'w').write(content)
-print('Fixed. Count:', content.count('#aab-export-panel.open'))
